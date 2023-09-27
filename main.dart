4397d312737021a54bada4ce6fd20d60b6e08fbd
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-09-26 21:45:50 -0300

    crinado a função  Menu

--- main.dart
+++ main.dart
@@ -1,2 +1,13 @@
-void main(){
+import 'dart:io';
+
+void main() {}
+
+void menu() {
+  print("Digite 1 para somar");
+  print("Digite 2 para subtrair");
+  print("Digite 3 para multiplicar");
+  print("Digite 4 para Dividir");
+  print("Digite 5 para sair");
+
+  String? entradaValor = stdin.readLineSync();
 }
